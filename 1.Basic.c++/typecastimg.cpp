1d921cf92fda999c0814e6d080c55a1b3c8d4751
Vashist Narayan Singh <vashistrajput777@gmail.com>
2026-01-17 15:29:11 +0530

    C++

--- 1.Basic.c++/typecastimg.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int x;
-    cin>>x;
-    float y = (float)x;
-    cout<<y/2;
-}
--- 4.Ternaryoperator/Q1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cout<<"enter n:";
-    cin>>n;
-    //(condition) ? if true : if false ;
-    (n%2==0) ? cout<<"even number" : cout<<"odd number";
-}
