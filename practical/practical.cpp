6bc5e3aa0cd474faf70bae1e690fcf7b695687d9
Vashist Narayan Singh <vashistrajput777@gmail.com>
2026-04-27 12:47:03 +0530

    Update practical.cpp

--- practical/practical.cpp
+++ practical/practical.cpp
@@ -1,9 +1,36 @@
 #include<iostream>
+#include <iostream>
 using namespace std;
-bool isPrime(int n){
-    if(n<=1) return false;
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0) return false;
+
+bool isPrime(int n) {
+    if (n <= 1) return false; // 0 and 1 are not prime
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
     }
     return true;
+}
+int reverseNumber(int n) {
+    int reversed = 0;
+    while (n > 0) {
+        int lastDigit = n % 10;
+        reversed = reversed * 10 + lastDigit;
+        n /= 10;
+    }
+    return reversed;
+}
+int main() {
+    int num;
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    int reversedNum = reverseNumber(num);
+    cout << "The reverse of " << num << " is " << reversedNum << "." << endl;
+
+    if (isPrime(reversedNum)) {
+        cout << reversedNum << " is a prime number." << endl;
+    } else {
+        cout << reversedNum << " is not a prime number." << endl;
+    }
+
+    return 0;
 }
