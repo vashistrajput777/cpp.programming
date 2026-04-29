79ca8c0ce39b758e1335af56558ef8b56a0a3d43
Vashist Narayan Singh <vashistrajput777@gmail.com>
2026-04-29 06:12:15 +0530

    Create practical2.cpp

---
+++ practical/practical2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal makes a sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() {
+        cout << "Dog barks" << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void sound() {
+        cout << "Cat meows" << endl;
+    }
+};
+
+int main() {
+    Animal* ptr;
+
+    Dog d;
+    Cat c;
+
+    ptr = &d;
+    ptr->sound();
+
+    ptr = &c;
+    ptr->sound();
+
+    return 0;
+}
